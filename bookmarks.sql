9530f2a9014c67c4845989d727ee4fa1f47d26ee
Gary Carmichael <g.carmichael90@gmail.com>
2015-12-06 18:38:38 +0000

    Operational.

--- bookmarks.sql
+++ bookmarks.sql
@@ -1,8 +1,8 @@
 drop table bookmarks;
 create table bookmarks (
   id serial4 primary key,
-  name VARCHAR(255) not null,
-  url VARCHAR(255) not null,
+  name VARCHAR(255),
+  url VARCHAR(255),
   genre VARCHAR(255),
   details VARCHAR(255)
 );
--- models/bookmark.rb
+++ models/bookmark.rb
@@ -1,9 +1,11 @@
 require_relative 'db_base'
 class Bookmark < DBBase
 
-  attributes ({:name => :string})
-  attributes ({:url => :string})
-  attributes ({:genre => :string})
-  attributes ({:details => :string})
+attributes( :url => :string, :genre => :string, :name => :string, :details => :string )
+
+  # attributes ({:name => :string})
+  # attributes ({:url => :string})
+  # attributes ({:genre => :string})
+  # attributes ({:details => :string})
 
 end
